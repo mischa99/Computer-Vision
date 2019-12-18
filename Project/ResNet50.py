34d201c284cbf09bf6cc85d0f483eb919b352834
TimmDL <44569885+TimmDL@users.noreply.github.com>
2019-12-18 17:10:34 +0100

    Add files via upload

--- Project/ResNet50.py
+++ Project/ResNet50.py
@@ -1,33 +1,20 @@
-from skimage.io import imread, imsave
-import matplotlib.pyplot as plt
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 13 12:02:32 2019
 
-#always do this before working with keras!
-import numpy as np
-np.random.seed(123)# um die Gewichte immer gleichzufaellig zu initialisieren
-
-import tensorflow as tf
-from tensorflow import set_random_seed
-set_random_seed(123)
-#tf.random.set_seed(123)
+@author: 7dill
+"""
 
 
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D, Flatten
+from keras.models import Sequential, Model
+from keras.layers import Dense, Conv2D, Flatten, Dropout, GlobalAveragePooling2D, Input
 from keras.utils import np_utils
 from keras.optimizers import SGD
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras import applications
+from keras.preprocessing.image import ImageDataGenerator
 
-import os
-from skimage.io import imread
-
-#Lambda-Layer vor dem ersten CNN Layer für resize images 
-'''
-original_dim = (32, 32, 3)
-target_size = (64, 64)
-input = keras.layers.Input(original_dim)
-x = tf.keras.layers.Lambda(lambda image: tf.image.resize(image, target_size))(input)
-'''
 
 train_datagen = ImageDataGenerator(
         rescale=1./255
@@ -39,7 +26,7 @@ val_datagen = ImageDataGenerator(
 
 train_generator = train_datagen.flow_from_directory(
     directory="/informatik2/students/home/7dill/Desktop/CV/Project/GroceryStoreDataset-V6_CNN/train/",
-    target_size=(256, 256),
+    target_size=(224, 224),
     color_mode="rgb",
     batch_size=16,
     class_mode="categorical",
@@ -49,7 +36,7 @@ train_generator = train_datagen.flow_from_directory(
 
 val_generator = val_datagen.flow_from_directory(
         directory="/informatik2/students/home/7dill/Desktop/CV/Project/GroceryStoreDataset-V6_CNN/val/",
-        target_size=(256, 256),
+        target_size=(224, 224),
         batch_size=16,
         class_mode='categorical',
         shuffle=True,
@@ -57,62 +44,32 @@ val_generator = val_datagen.flow_from_directory(
         )
 
 #include_top = false -> not including output layers because we need to fit the model on our own problem 
-model = applications.resnet50.ResNet50(weights= None, include_top=False, input_shape= (img_height,img_width,3))
+new_input = Input(shape=(224,224,3))
+model = applications.resnet50.ResNet50(weights='imagenet', include_top=False,input_tensor=new_input)
+
+# mark loaded layers as not trainable
+for layer in model.layers:
+	layer.trainable = False
+    
+# adding our own classifier/output layers
 x = model.output
 x = GlobalAveragePooling2D()(x)
-#x = Dropout(0.2)(x)
-predictions = Dense (16, activation= 'softmax')(x)
+x = Dropout(0.2)(x)
+fc1 = Dense(256, activation='relu')(x)
+fc2 = Dense(256, activation='relu')(fc1)
+predictions = Dense (16, activation= 'softmax')(fc2)
 model = Model(inputs = model.input, outputs = predictions)
 
 model.compile(loss='categorical_crossentropy',
 optimizer=SGD(lr=0.01, momentum=0.9), metrics=['accuracy'])
 
-model.fit_generator(train_generator,steps_per_epoch=100, epochs=10, validation_data=val_generator,
-          verbose=1, callbacks=[EarlyStopping(monitor='valloss',min_delta=0, patience=3),
+model.fit_generator(train_generator, epochs=10, validation_data=val_generator,
+          verbose=1, callbacks=[EarlyStopping(monitor='val_loss',min_delta=0, patience=3),
         ModelCheckpoint(filepath='/informatik2/students/home/7dill/Desktop/CV/Project/cnn1weights.h5',
          monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=False, mode='auto', period=1)])
 
-
-test_datagen = ImageDataGenerator(
-        rescale=1./255
-        )
-
-test_generator = train_datagen.flow_from_directory(
-    directory="/informatik2/students/home/7dill/Desktop/CV/Project/GroceryStoreDataset-V6_CNN/test/",
-    target_size=(256, 256),
-    color_mode="rgb",
-    batch_size=16,
-    class_mode="categorical",
-    shuffle=True,
-)
-
-val_loss, val_acc = model.evaluate_generator(test_generators,steps=100,
+model.load_weights('ResNet50weights.h5', by_name=True)
+val_loss, val_acc = model.evaluate_generator(val_generator,
  callbacks=None, max_queue_size=10, use_multiprocessing=False, verbose=1)
 
 print(val_loss, val_acc)
-
-
-
-
-
-
-
-
-
-
-'''
-DATADIR = 
-CATEGORIES = ["Apple", "Aubergine", "Banana", "Carrots", "Cucumber", "Ginger", "Lemon", "Melon", "Orange", "Pear",
-            "Pepper","Pineapple", "Potato", "Tomato", "Watermelon", "Zucchini"]
-
-for category in CATEGORIES:
-    path= os.path.join(DATADIR, category) # path to Image Class dir 
-    for img in os.listdir(path):
-        img_array=imread(os.path.join(path,img))
-        plt.imshow(img_array)
-        plt.show()
-        break
-    break
-'''
-
-
---
+++ Project/VGG16.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 18 15:14:49 2019
+
+@author: 7dill
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 11 15:00:30 2019
+
+@author: 7dill
+"""
+from keras.models import Sequential, Model
+from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropout, Input,GlobalAveragePooling2D
+from keras.utils import np_utils, plot_model
+from keras.optimizers import SGD
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.preprocessing.image import ImageDataGenerator
+from keras import applications
+
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255
+        )
+
+val_datagen = ImageDataGenerator(
+        rescale=1./255
+        )
+
+test_datagen = ImageDataGenerator(
+        rescale=1./255
+        )
+
+train_generator = train_datagen.flow_from_directory(
+    directory="/informatik2/students/home/7dill/Desktop/CV/Project/GroceryStoreDataset-V6_CNN/train/",
+    target_size=(224, 224),
+    color_mode="rgb",
+    batch_size=8,
+    class_mode="categorical",
+    shuffle=True,
+    
+)
+
+val_generator = val_datagen.flow_from_directory(
+        directory="/informatik2/students/home/7dill/Desktop/CV/Project/GroceryStoreDataset-V6_CNN/val/",
+        target_size=(224, 224),
+        batch_size=8,
+        class_mode='categorical',
+        shuffle=True,
+        )
+'''
+test_generator = test_datagen.flow_from_directory(
+        directory="/informatik2/students/home/7dill/Desktop/CV/Project/GroceryStoreDataset-V6_CNN/test/",
+        target_size=(224, 224),
+        batch_size=8,
+        class_mode='categorical',
+        shuffle=True,
+        )
+'''
+new_input = Input(shape=(224,224,3))
+model = applications.vgg16.VGG16(weights='imagenet', input_tensor=new_input, include_top=False)
+#print(model.summary())
+#plot_model(model,to_file='vgg.png')
+
+#predictions = model.predict_generator(test_generator,steps=None, callbacks=None, max_queue_size=10, workers=1, use_multiprocessing=False, verbose=1)
+#print(predictions)
+
+# mark loaded layers as not trainable
+for layer in model.layers:
+	layer.trainable = False
+
+# add new classifier layers
+#x = GlobalAveragePooling2D()(model.output)
+flat1 = Flatten()(model.output)
+fc1 = Dense(256, activation='relu')(flat1)
+fc2 = Dense(256, activation='relu')(fc1)
+output = Dense(16, activation='softmax')(fc2)
+# define new model
+model = Model(inputs=model.inputs, outputs=output)
+
+#CNN komplieren
+model.compile(loss='categorical_crossentropy',
+optimizer=SGD(lr=0.01, momentum=0.9), metrics=['accuracy'])
+
+#CNN trainieren
+model.fit_generator(train_generator, epochs=10, validation_data=val_generator,
+          verbose=1, callbacks=[EarlyStopping(monitor='val_loss',min_delta=0, patience=3),
+        ModelCheckpoint(filepath='/informatik2/students/home/7dill/Desktop/CV/Project/cnn2weights.h5', 
+        monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=False, mode='auto', period=1)])
+
+model.load_weights('VGG16weights.h5', by_name=True)
+
+val_loss, val_acc = model.evaluate_generator(val_generator,
+ callbacks=None, max_queue_size=10, use_multiprocessing=False, verbose=1)
+print(val_loss, val_acc)
